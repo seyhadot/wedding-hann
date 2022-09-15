ed1fa2e758da7482270ab5f5e272dba011273dc8
seyhadot <seyhadot@gmail.com>
2022-09-15 15:47:18 +0700

    deno freshing template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,33 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "dscountdown";
-import "jquery.countdown";
 
-import jquery from "jquery";
-window.jQuery = jquery;
-window.$ = jquery;
+// The data/time we want to countdown to
+var countDownDate = new Date("Jan 7, 2023 19:00:00").getTime();
 
-$(function () {
-  $(".demo1").dsCountDown({
-    endDate: new Date("December 23, 2017 17:59:00"),
-  });
-});
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+
+// Calculating the days, hours, minutes and seconds left
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+// Result is output to the specific element
+document.getElementById("days").innerHTML = days + "d "
+document.getElementById("hours").innerHTML = hours + "h "
+document.getElementById("mins").innerHTML = minutes + "m "
+document.getElementById("secs").innerHTML = seconds + "s "
+
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("days").innerHTML = ""
+    document.getElementById("hours").innerHTML = ""
+    document.getElementById("mins").innerHTML = ""
+    document.getElementById("secs").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+
